4bb760db4c8578dba32642a24be901d3967aa040
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-10-28 19:17:15 +0200

    nimikortit.py: Move card creation into separate function

--- nimikortit.py
+++ nimikortit.py
@@ -32,25 +32,29 @@ fonts=[]
 for i in range(40):
 	fonts.append((ImageFont.truetype(fontti_filename, 500-10*i), 500))
 
+def create_card(name):
+	if len(name) < 2:
+		return
+
+	print(name)
+
+	imgCpy = img.copy()
+	draw = ImageDraw.Draw(imgCpy)
+	fontId = 0
+	w, h = draw.textsize(name,font=fonts[fontId][0])
+	w -= fonts[fontId][1]/10
+	while w > imgW-2*border_size:
+		fontId += 1
+		w, h = draw.textsize(name,font=fonts[fontId][0])
+		w -= fonts[fontId][1]/10
+
+	draw.text(((imgW/2)-(w/2), (imgH/2)-(h/2)), name, font_color, font=fonts[fontId][0])
+	imgCpy.save(output_dir + "/nimikortti - "+name+".png")
+
 if (os.path.isdir(output_dir) == False):
     os.makedirs(output_dir)
 
 for row in plassi:
 	for name in row:
-		if len(name) < 2:
-			continue
+		create_card(name)
 
-		print(name)
-
-		imgCpy = img.copy()
-		draw = ImageDraw.Draw(imgCpy)
-		fontId = 0
-		w, h = draw.textsize(name,font=fonts[fontId][0])
-		w -= fonts[fontId][1]/10
-		while w > imgW-2*border_size:
-			fontId += 1
-			w, h = draw.textsize(name,font=fonts[fontId][0])
-			w -= fonts[fontId][1]/10
-
-		draw.text(((imgW/2)-(w/2), (imgH/2)-(h/2)), name, font_color, font=fonts[fontId][0])
-		imgCpy.save(output_dir + "/nimikortti - "+name+".png")
