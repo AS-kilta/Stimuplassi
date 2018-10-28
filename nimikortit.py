bfb2dcdc6f492db2ac8064156986406e04c5f648
Lehdari <miika.lehtimaki@gmail.com>
2018-10-28 14:58:25 +0200

    nimikortit.py: add configuration options

--- nimikortit.py
+++ nimikortit.py
@@ -3,15 +3,28 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 import csv
 
-plassi = list(csv.reader(open("plassi.csv")))
+
+# CSV-muotoisen plassin tiedostonimi
+plassi_filename = "plassi.csv"
+# Taustan tiedostonimi
+tausta_filename = "nimikortti.png"
+# Fontin tiedostonimi
+fontti_filename = "Chelsea III.ttf"
+# Reunuksen koko pikseleinä (eli paljonko jää tilaa tekstin ja laitojen välille)
+border_size = 350
+# Fontin väri (0-255 RGB)
+font_color = (255,255,255)
+
+
+plassi = list(csv.reader(open(plassi_filename)))
 print(len(plassi))
 print(len(plassi[0]))
 
-img = Image.open("nimikortti.png")
+img = Image.open(tausta_filename)
+imgW, imgH = img.size
 fonts=[]
-for i in range(30):
-	fonts.append((ImageFont.truetype("Chelsea III.ttf", 500-10*i), 500))
-
+for i in range(40):
+	fonts.append((ImageFont.truetype(fontti_filename, 500-10*i), 500))
 
 for row in plassi:
 	for name in row:
@@ -25,10 +38,10 @@ for row in plassi:
 		fontId = 0
 		w, h = draw.textsize(name,font=fonts[fontId][0])
 		w -= fonts[fontId][1]/10
-		while w > 3800:
+		while w > imgW-2*border_size:
 			fontId += 1
 			w, h = draw.textsize(name,font=fonts[fontId][0])
 			w -= fonts[fontId][1]/10
 
-		draw.text((2250-(w/2), 750-(h/2)),name,(255,255,255),font=fonts[fontId][0])
+		draw.text(((imgW/2)-(w/2), (imgH/2)-(h/2)), name, font_color, font=fonts[fontId][0])
 		imgCpy.save("nimikortit/nimikortti - "+name+".png")
